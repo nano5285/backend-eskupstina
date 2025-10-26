69ecb90b1885c5e652cf69be51f4682b8df6d764
nano5285 <luciano.grmovsek@protonmail.com>
2025-10-26 23:17:01 +0100

    Refactor API routes and add new controllers

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,21 +1,56 @@
-import AUTH from "./auth";
-import Agenda from "./agenda"
-import Vote from "./vote"
+import { Router } from "express";
+import {
+  createAgenda,
+  getAgendas,
+  getAgendaById,
+  deleteAgenda,
+  getAgendaPdf,
+  getAgendaPdfBlob,
+} from "../controllers/agenda";
+import {
+  login,
+  register,
+  getUsers,
+  deleteUser,
+  getTvUsers,
+} from "../controllers/auth";
+import {
+  startVote,
+  handleVote,
+  closeVote,
+  resetVote,
+} from "../controllers/vote";
+import {
+  getLatestSessionOrActive,
+  getSessionsByYear,
+} from "../controllers/session"; // <-- 1. UVEZI NOVE KONTROLERE
 
-const API = (router: any) => {
-    // APIs for Auth
-    router.get('/test', (req: any, res: any) => {
-        res.send("Hello World!");
-    });
-    router.post("/login", AUTH.login);
-    router.get("/get_agenda", Agenda.get_agenda);
-    router.post("/users", AUTH.get_users);
-    router.get("/pdf", Agenda.show_pdf);
-    router.post("/vote", Agenda.do_vote);
-    router.get("/get_vote", Vote.get_vote);
-    router.post("/start_vote", Agenda.start_vote);
-    router.post("/close_vote", Agenda.close_vote);
-    router.post("/reset_vote", Agenda.reset_vote);
-};
+const router = Router();
 
-export default API;
+// Auth
+router.post("/auth/login", login);
+router.post("/auth/register", register);
+router.post("/auth/get-users", getUsers);
+router.post("/auth/get-tv-users", getTvUsers);
+router.post("/auth/delete", deleteUser);
+
+// Agenda
+router.post("/agenda/create", createAgenda);
+router.get("/agenda/all", getAgendas);
+router.get("/agenda", getAgendaById);
+router.post("/agenda/delete", deleteAgenda);
+router.get("/pdf", getAgendaPdf);
+router.get("/pdf-blob", getAgendaPdfBlob);
+
+// Vote
+router.post("/vote/start", startVote);
+router.post("/vote/handle", handleVote);
+router.post("/vote/close", closeVote);
+router.post("/vote/reset", resetVote);
+
+// Sessions (Sjednice)
+// <-- 2. DODAJ NOVE RUTE OVDJE
+router.get("/get_session_or_latest", getLatestSessionOrActive);
+router.get("/sessions_by_year", getSessionsByYear);
+
+export default router;

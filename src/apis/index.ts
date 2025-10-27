600e5def2038830365bf16b137326de563d749be
Asad Abbas <abbasasad057@gmail.com>
2025-10-27 13:20:54 +0500

    reverted backend changes and added missing endpoints

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,21 +1,28 @@
 import AUTH from "./auth";
-import Agenda from "./agenda"
-import Vote from "./vote"
+import Agenda from "./agenda";
+import Vote from "./vote";
+import {
+  getLatestSessionOrActive,
+  getSessionsByYear,
+} from "../controllers/session";
 
 const API = (router: any) => {
-    // APIs for Auth
-    router.get('/test', (req: any, res: any) => {
-        res.send("Hello World!");
-    });
-    router.post("/login", AUTH.login);
-    router.get("/get_agenda", Agenda.get_agenda);
-    router.post("/users", AUTH.get_users);
-    router.get("/pdf", Agenda.show_pdf);
-    router.post("/vote", Agenda.do_vote);
-    router.get("/get_vote", Vote.get_vote);
-    router.post("/start_vote", Agenda.start_vote);
-    router.post("/close_vote", Agenda.close_vote);
-    router.post("/reset_vote", Agenda.reset_vote);
+  // APIs for Auth
+  router.get("/test", (req: any, res: any) => {
+    res.send("Hello World!");
+  });
+  router.post("/login", AUTH.login);
+  router.get("/get_agenda", Agenda.get_agenda);
+  router.post("/users", AUTH.get_users);
+  router.get("/pdf", Agenda.show_pdf);
+  router.post("/vote", Agenda.do_vote);
+  router.get("/get_vote", Vote.get_vote);
+  router.post("/start_vote", Agenda.start_vote);
+  router.post("/close_vote", Agenda.close_vote);
+  router.post("/reset_vote", Agenda.reset_vote);
+
+  router.get("/get_session_or_latest", getLatestSessionOrActive);
+  router.get("/sessions_by_year", getSessionsByYear);
 };
 
 export default API;
